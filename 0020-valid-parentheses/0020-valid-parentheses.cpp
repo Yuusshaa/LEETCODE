103a4792b067d94420f8b7f96e9d1c424b08f08d
Yusha <charbat123@gmail.com>
2026-08-01 18:18:47 +0500

    Time: 0 ms (100%), Space: 8.7 MB (98.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> openBrackets;
+        vector<char> openBrackets;
 
         for (char c : s) {
-
+           
             if (c == '(' || c == '[' || c == '{') {
-                openBrackets.push(c);
+                openBrackets.push_back(c);
             }
+
             else {
+ 
                 if (openBrackets.empty()) return false;
 
-                char top = openBrackets.top();
+                char top = openBrackets.back();
 
                 if ((c == ')' && top != '(') ||
                     (c == ']' && top != '[') ||
                     (c == '}' && top != '{')) {
-                    return false;
+                    return false; 
                 }
 
-                openBrackets.pop(); 
+                openBrackets.pop_back(); 
             }
         }
+
         return openBrackets.empty();
     }
 };
