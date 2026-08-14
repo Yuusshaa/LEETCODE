6ca8d1a21f6e26cf057424ad59d0bf18d20dd665
Yusha <charbat123@gmail.com>
2026-08-14 05:40:26 +0500

    Time: 0 ms (100%), Space: 8.1 MB (15.48%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int width = (int)sqrt((double)area);
+
+        while (area % width != 0) {
+            width--;
+        }
+
+        int length = area / width;
+        return {length, width};
+    }
+};
