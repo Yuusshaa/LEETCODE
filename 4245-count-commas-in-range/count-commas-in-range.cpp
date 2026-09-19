5705e0a59edb8220caa61b1888aacff191d35060
Yusha <charbat123@gmail.com>
2026-09-20 04:22:15 +0500

    Time: 8 ms (6.09%) | Memory: 8.6 MB (14.56%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<1000)
+        {
+            return 0;
+        }
+        int count = 0;
+        for(int i = 0; i<=n; i++)
+        {
+            if(i >= 1000)
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
